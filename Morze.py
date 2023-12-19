fef2a41dc2ae05d5ee23b89cf5de6eb41c57888e
Ghost_Boy <129123736+Hacker273020@users.noreply.github.com>
2023-12-19 11:01:38 -0600

    Morze.py

--- Morze.py
+++ Morze.py
@@ -1,18 +1,20 @@
+import os
+
 MORSE = {'A': '.-', 'B': '-...',
-                   'C': '-.-.', 'D': '-..', 'E': '.',
-                   'F': '..-.', 'G': '--.', 'H': '....',
-                   'I': '..', 'J': '.---', 'K': '-.-',
-                   'L': '.-..', 'M': '--', 'N': '-.',
-                   'O': '---', 'P': '.--.', 'Q': '--.-',
-                   'R': '.-.', 'S': '...', 'T': '-',
-                   'U': '..-', 'V': '...-', 'W': '.--',
-                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
-                   '1': '.----', '2': '..---', '3': '...--',
-                   '4': '....-', '5': '.....', '6': '-....',
-                   '7': '--...', '8': '---..', '9': '----.',
-                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
-                   '?': '..--..', '/': '-..-.', '-': '-....-',
-                   '(': '-.--.', ')': '-.--.-', ' ': '/'}
+         'C': '-.-.', 'D': '-..', 'E': '.',
+         'F': '..-.', 'G': '--.', 'H': '....',
+         'I': '..', 'J': '.---', 'K': '-.-',
+         'L': '.-..', 'M': '--', 'N': '-.',
+         'O': '---', 'P': '.--.', 'Q': '--.-',
+         'R': '.-.', 'S': '...', 'T': '-',
+         'U': '..-', 'V': '...-', 'W': '.--',
+         'X': '-..-', 'Y': '-.--', 'Z': '--..',
+         '1': '.----', '2': '..---', '3': '...--',
+         '4': '....-', '5': '.....', '6': '-....',
+         '7': '--...', '8': '---..', '9': '----.',
+         '0': '-----', ', ': '--..--', '.': '.-.-.-',
+         '?': '..--..', '/': '-..-.', '-': '-....-',
+         '(': '-.--.', ')': '-.--.-', ' ': '/'}
 
 def texto_a_morse(texto):
     texto = texto.upper()
@@ -21,29 +23,35 @@ def texto_a_morse(texto):
 
 def morse_a_texto(morse_code):
     morse_code = morse_code.split(' ')
-    texto = ''.join([key for value, key in MORSE_CODE_DICT.items() if value in morse_code])
+    texto = ''.join([key for value, key in MORSE.items() if value in morse_code])
     return texto
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
     while True:
+        limpiar_pantalla()
         print("1. Texto a Morse")
         print("2. Morse a Texto")
         print("0. Salir")
-        
+
         opcion = input("Elige una opción: ")
-        
+
         if opcion == '0':
             break
         elif opcion == '1':
             texto = input("Ingresa el Texto: ")
             morse_code = texto_a_morse(texto)
+            limpiar_pantalla()
             print("Texto en Morse:", morse_code)
+            input("Presiona Enter para continuar...")
         elif opcion == '2':
             morse_code = input("Ingresa el código Morse: ")
             texto = morse_a_texto(morse_code)
+            limpiar_pantalla()
             print("Texto descifrado:", texto)
-        else:
-            print("Opción no válida")
+            input("Presiona Enter para continuar...")
 
 if __name__ == "__main__":
     main()
